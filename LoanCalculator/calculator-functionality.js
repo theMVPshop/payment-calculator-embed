e1fd6b0bea451aa5171461151b0e2962bcdf5555
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-10-06 19:14:21 -0500

    20211006 Added basic calculation functionality

--- LoanCalculator/calculator-functionality.js
+++ LoanCalculator/calculator-functionality.js
@@ -4,7 +4,7 @@ var loanAmount = document.getElementById("loan-amount");
 var loanDisplay = document.getElementById("display-loan-amount");
 loanDisplay.innerHTML = "$" + loanAmount.value;
 
-loanAmount.oninput = function(){
+loanAmount.oninput = function () {
     loanDisplay.innerHTML = "$" + this.value;
 }
 
@@ -13,7 +13,7 @@ var depositAmount = document.getElementById("deposit-amount");
 var depositDisplay = document.getElementById("display-deposit-amount");
 depositDisplay.innerHTML = "$" + depositAmount.value;
 
-depositAmount.oninput = function(){
+depositAmount.oninput = function () {
     depositDisplay.innerHTML = "$" + this.value;
 }
 
@@ -22,17 +22,16 @@ var ppfAmount = document.getElementById("ppf-amount");
 var ppfDisplay = document.getElementById("display-ppf-amount");
 ppfDisplay.innerHTML = "$" + ppfAmount.value;
 
-ppfAmount.oninput = function(){
+ppfAmount.oninput = function () {
     ppfDisplay.innerHTML = "$" + this.value;
 }
-
 
 //Display current Installment Number
 var installmentSlider = document.getElementById("installment-range");
 var installmentDisplay = document.getElementById("display-installments-amount");
 installmentDisplay.innerHTML = installmentSlider.value;
 
-installmentSlider.oninput = function(){
+installmentSlider.oninput = function () {
     installmentDisplay.innerHTML = this.value;
 }
 
@@ -41,14 +40,12 @@ var bimonthlySwitch = document.getElementById("bimonthly-switch");
 var bimonthlyDisplay = document.getElementById("display-installment-type");
 bimonthlyDisplay.innerHTML = "Monthly";
 
-bimonthlySwitch.oninput = function(){
+bimonthlySwitch.oninput = function () {
 
-    if(bimonthlySwitch.checked)
-    {
+    if (bimonthlySwitch.checked) {
         bimonthlyDisplay.innerHTML = "Bi-Monthly";
     }
-    else
-    {
+    else {
         bimonthlyDisplay.innerHTML = "Monthly";
     }
 }
@@ -57,35 +54,51 @@ bimonthlySwitch.oninput = function(){
 //Formula: (tuition + PPF) - deposit / installments
 //if deposit amount < 2490, ppf = 790
 
-//Number Variables
-var calcInstallments;
-var calcInstallmentsAmount;
-var calcPPF;
-//The amount of installments that must be paid before starting the course
-var calcPaymentsToStart;
+var totalPerInstallmentAmount;
 
 //Display variables
+//Installment amount 
 var calcInstallmentsDisplay = document.getElementById("display-calculated-amount-installments");
+//Money amount
 var calcInstallmentsAmountDisplay = document.getElementById("display-calculated-amount-perinstallments");
 var calcPPF = document.getElementById("display-calculated-installments");
+
+var owedToStart = document.getElementById("owedToStart");
 var calcPaymentsToStartDisplay = document.getElementById("display-calculated-payments-perinstallments");
 
+function calculateButton() {
+    //if the payments are bimonthly
+    if (bimonthlySwitch.checked) {
 
+        totalPerInstallmentAmount =
+            ((parseFloat(loanAmount.value) + parseFloat(ppfAmount.value)) - parseFloat(depositAmount.value)) / parseFloat(installmentSlider.value * 2);
+        calcInstallmentsAmountDisplay.innerHTML =  "$" + totalPerInstallmentAmount;
 
-var calculateButton = document.getElementById("calculate-button");
+        calcInstallmentsDisplay.innerHTML = parseFloat(installmentSlider.value * 2);
 
-calculateButton.oninput = function()
-{
-//if the payments are bimonthly
-if(bimonthlySwitch.checked)
-{
-    calcInstallmentsAmountDisplay.innerHTML = (loanAmount + ppfAmount) - depositAmount / (installmentSlider / 2);
-}
-//if payments are monthly
+        //OwedToStart
+        calcPaymentsToStartDisplay.innerHTML = parseFloat(owedToStart.value) / totalPerInstallmentAmount * 2;
+    }
+    //if payments are monthly
+    else {
+        totalPerInstallmentAmount =
+            (parseFloat(loanAmount.value) + parseFloat(ppfAmount.value) - parseFloat(depositAmount.value)) / (parseFloat(installmentSlider.value))
+        calcInstallmentsAmountDisplay.innerHTML = "$" + totalPerInstallmentAmount;
 
-else
-{
-    calcInstallmentsAmountDisplay.innerHTML = (loanAmount + ppfAmount) - depositAmount / (installmentSlider);
-}
+        calcInstallmentsDisplay.innerHTML = parseFloat(installmentSlider.value);
+
+        //OwedToStart
+        calcPaymentsToStartDisplay.innerHTML = parseFloat(owedToStart.value) / totalPerInstallmentAmount;
+    }
+
+    //Calculate PPF WIP
+    if (totalPerInstallmentAmount < 2490)
+    {
+        calcPPF.innerHTML = "$" + 790;
+    }
+    else
+    {
+        calcPPF.innerHTML = "$" +  0;
+    }
 }
 
