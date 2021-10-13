82a81e2c579fd91afcb105783858c477584f3b5c
Andrew Brewer <72884247+andrewbrewer22@users.noreply.github.com>
2021-10-12 19:30:32 -0500

    20211012 Added calculations, date functionality

--- LoanCalculator/calculator-functionality.js
+++ LoanCalculator/calculator-functionality.js
@@ -1,5 +1,6 @@
-var tuition;
 var ppf;
+var currentPPF;
+var wavePPFAmount
 //Used for user affordable payment amount
 var deposit;
 var depositMax;
@@ -11,10 +12,21 @@ var monthlyAmountMin;
 
 var programLength;
 var programCost;
+var programLeftOverCost;
+
+//placeholder variables for calculating length, how early, and the when to finish classes
+var date = new Date();
+var monthsToPay = 0;
+var currentDate;
+var earlyStartDate;
+var earlyEndDate;
+
 //DOM
+var todaysDate = document.getElementById("current-date");
 
 var programLengthObject = document.getElementById("inner-program-length");
 var programCostObject = document.getElementById("inner-cost-of-program");
+var wavePPFElement = document.getElementById("minimum-ppf-wave-amount");
 
 //Input Boxes
 var userDepositBox = document.getElementById("down-payment-box");
@@ -24,29 +36,112 @@ var userMonthlyPaymentsBox = document.getElementById("monthly-payment-box");
 var userDepositSlider = document.getElementById("down-payment-slider");
 var userMonthlyPaymentsSlider = document.getElementById("monthly-payment-slider");
 
-var monthsToPay = document.getElementById("months-to-pay");
-var earlyStartDate = document.getElementById("earliest-start-date");
-var earlyEndDate = document.getElementById("earliest-end-date");
+var monthsToPayObject = document.getElementById("months-to-pay");
+var earlyStartDateObject = document.getElementById("earliest-start-date");
+var earlyEndDateObject = document.getElementById("earliest-end-date");
 
 //assign hard code values
+//program length in weeks
 programLength = 38;
 programLengthObject.innerHTML = programLength + " Weeks";
 
 programCost = 18500;
 ppf = 750;
-programCostObject.innerHTML ="$" + programCost + " + $" + ppf + "*";
+//The amount that must be deposited before ppf fee is waved
+wavePPFAmount = 2499;
+
+wavePPFElement.innerHTML = wavePPFAmount;
+programCostObject.innerHTML = "$" + programCost + " + $" + ppf + "*";
+
+//set min and max values for user input
+depositMin = 500;
+depositMax = programCost;
+
+monthlyAmountMin = 100;
+monthlyAmountMax = 500;
+
+userDepositBox.min = depositMin;
+userDepositBox.max = depositMax;
+userDepositSlider.min = depositMin;
+userDepositSlider.max = depositMax;
+
+userMonthlyPaymentsBox.min = monthlyAmountMin;
+userMonthlyPaymentsBox.max = monthlyAmountMax;
+userMonthlyPaymentsSlider.min = monthlyAmountMin;
+userMonthlyPaymentsSlider.max = monthlyAmountMax;
 
 //Put Slider numbers into input boxes, and vice versa
-userDepositSlider.oninput = function(){
+userDepositSlider.oninput = function () {
     userDepositBox.value = this.value;
 }
-userDepositBox.oninput = function(){
+userDepositBox.oninput = function () {
     userDepositSlider.value = this.value;
 }
 
-userMonthlyPaymentsSlider.oninput = function(){
-    userMonthlyPaymentsBox = this.value;
+userMonthlyPaymentsSlider.oninput = function () {
+    userMonthlyPaymentsBox.value = this.value;
 }
-userMonthlyPaymentsBox.oninput = function(){
-    userMonthlyPaymentsSlider = this.value;
+userMonthlyPaymentsBox.oninput = function () {
+    userMonthlyPaymentsSlider.value = this.value;
+}
+
+//Set dates
+currentDate = date.getFullYear() + "-" + (date.getMonth() + 1);
+todaysDate.innerHTML = currentDate;
+
+function calcValues() {
+
+    //recalculate calculations to display in html
+    monthsToPayObject.innerHTML = monthsToPay + " months";
+    earlyStartDateObject.innerHTML = earlyStartDate;
+    earlyEndDateObject.innerHTML = earlyEndDate;
+
+    //(tuition + ppf) - deposit = left over
+    //left over / payments per month = amount of months
+    programLeftOverCost = (programCost + currentPPF) - deposit;
+
+    monthsToPay = Math.trunc(programLeftOverCost / monthlyAmount);
+
+    //Update our data on input up
+    deposit = userDepositBox.value;
+    monthlyAmount = userMonthlyPaymentsBox.value;
+
+    //calculate PPF
+    if(deposit < wavePPFAmount)
+    {
+        currentPPF = ppf;
+    }
+    else
+    {
+        currentPPF = 0;
+    }
+
+    //earliest start date. If our calc is negative, class can be started now because
+    //you will finish paying before the program finishes
+    var weeksToPay = (monthsToPay * 4) - programLength;
+    if(weeksToPay <= 0)
+    {
+        earlyStartDate = currentDate;
+    }
+    else
+    {
+        //years do not progress with (months - program length)
+
+        var newDate = new Date();
+        newDate.setDate(newDate.getDate() + (monthsToPay - programLength));
+        var mm = newDate.getMonth() + 1;
+        var yyyy = newDate.getFullYear();
+        var earlyStartDateString = yyyy + "-" + mm;
+
+        earlyStartDate = earlyStartDateString;
+    }
+
+    var newDate = new Date();
+    newDate.setDate(newDate.getDate() + monthsToPay);
+    var mm = newDate.getMonth() + 1;
+    var yyyy = newDate.getFullYear();
+    var earlyEndDateString = yyyy + "-" + mm;
+
+    earlyEndDate = earlyEndDateString; 
+
 }
